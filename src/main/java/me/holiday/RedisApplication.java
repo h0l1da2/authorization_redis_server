624d33d4781780822f404a21b5cb58971e81a213
h0l1da2 <holiday.k1@icloud.com>
2024-12-10 01:23:21 +0900

    feat(main): 메인 클래스 변경
    - me.holiday 부터 잡도록 수정
    
    holiday

--- src/main/java/me/holiday/RedisApplication.java
+++ src/main/java/me/holiday/RedisApplication.java
@@ -1,9 +1,11 @@
-package me.holiday.redis;
+package me.holiday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "me.holiday")
+@ConfigurationPropertiesScan(basePackages = "me.holiday")
 public class RedisApplication {
 
 	public static void main(String[] args) {
