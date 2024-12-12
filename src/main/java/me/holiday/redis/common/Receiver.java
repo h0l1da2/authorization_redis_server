677dd389d1ceeddb2b3ea45b8288952eb3e78f35
h0l1da2 <holiday.k1@icloud.com>
2024-12-12 16:00:22 +0900

    refactor(redis): Receiver 로그 변경
    holiday

--- src/main/java/me/holiday/redis/common/Receiver.java
+++ src/main/java/me/holiday/redis/common/Receiver.java
@@ -10,7 +10,7 @@ public class Receiver {
     private final AtomicInteger counter = new AtomicInteger();
 
     public void receiveMessage(String message) {
-        log.info("[Receiver] < {} >", message);
+        log.info("[Redis] 새 메시지 수신");
         counter.incrementAndGet();
     }
 
